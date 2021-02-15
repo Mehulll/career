f502d1f721594af6ded75c73efb2a9a201f8e5b1
Mehulll <mehul.aryan.2222@gmail.com>
2021-02-15 11:55:05 +0530

    oldfix

--- server.js
+++ server.js
@@ -3,26 +3,30 @@ const connectDB = require('./DB/Connection');
 const cors = require('cors')
 const morgan = require('morgan')
 const app = express();
-
+const path = require('path')
 connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
-// HTTP request logger
-app.use(morgan('tiny'))
 
-app.use('/api/userModel', require('./Api/User'));
-// app.use(cors());
-const Port = process.env.Port || 5000;
-
-if(process.env.NODE_ENV === 'production') {
+if(process.env.NODE_ENV === 'development') {
   app.use(express.static(path.join(__dirname, 'client', 'build')));
+  console.log('ahello')
 
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
   });
   
 }
+
+// HTTP request logger
+app.use(morgan('tiny'))
+
+app.use('/api/userModel', require('./Api/User'));
+// app.use(cors());
+const Port = process.env.Port || 5000;
+console.log('bad')
+// console.log(process.env)
 
 app.listen(Port, () => console.log(`Server started ${Port}`));
 
