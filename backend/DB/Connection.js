8bb9c0dc66fa8ceb7cf2889f1278a9497a683934
developer19m <42600672+Mehulll@users.noreply.github.com>
2022-03-28 13:09:14 +0530

    Update Connection.js

--- backend/DB/Connection.js
+++ backend/DB/Connection.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const URI ="mongodb+srv://MehulJ:2300900M@cluster0.bix28.mongodb.net/careers?retryWrites=true&w=majority";
-
 const connectDB = async () => {
   await mongoose.connect(process.env.MONGODB_URI || URI, {
     useUnifiedTopology: true,
